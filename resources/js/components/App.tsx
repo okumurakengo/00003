89e32a22a7944c3add29096e0e6cf1654940f975
okumurakengo <okumurakengo1a@gmail.com>
2018-12-06 14:01:41 +0000

    typescriptを実行

---
+++ resources/js/components/App.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-8">
+                        <div className="card">
+                            <div className="card-header">Example Component</div>
+
+                            <div className="card-body">
+                                I'm an example component!
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+render(<App/>, document.getElementById('app') as HTMLDivElement);
